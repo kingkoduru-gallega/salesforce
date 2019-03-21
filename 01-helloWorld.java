7425b1303f5eba571eebf1f3ea47257fb1bdb1a0
kingkoduru-gallega <48802011+kingkoduru-gallega@users.noreply.github.com>
2019-03-21 12:15:15 -0400

    Add files via upload

--- 01-helloWorld.java
+++ 01-helloWorld.java
@@ -3,10 +3,10 @@ package packSF1;
 public class helloWorld {
 
 	public static void main(String[] args) {
-	public static void run() {
+	//public static void run() {
 		
 		System.out.print("Hello World");
-		System.debug('Hello World');
+		//System.debug('Hello World');
 
 	}
 
